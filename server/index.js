31523146829b0a0ad750428f593ad2843138b1a7
Kavita <49744059+KavitaSomaiya@users.noreply.github.com>
2019-09-19 15:17:31 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import expressGraphQL from "express-graphql";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+require("dotenv").config();
+import { GraphQLServer, PubSub } from "graphql-yoga";
+import mongoose from "mongoose";
+
+import schema from "../graphql/";
+import { models } from "./config/db/";
+
+const { mongoURI: db } = process.env;
+
+const pubsub = new PubSub();
+
+const options = {
+  port: process.env.PORT || "4000",
+  endpoint: "/graphql",
+  subscriptions: "/subscriptions",
+  playground: "/playground"
+};
+
+const context = {
+  models,
+  pubsub
+};
+
+// Connect to MongoDB with Mongoose.
+
+const app = express();
+const PORT = process.env.PORT || "4000";
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://DataBase-1:1234@mycluster-71u5b.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const server = new GraphQLServer({
+  schema,
+  context
+});
+
+server.start(options, ({ port }) => {
+  console.log(`🚀 Server is running on http://localhost:${port}`);
+});
